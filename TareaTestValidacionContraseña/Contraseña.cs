666adfa3d4acc61ce65a777616fa17ea42801910
david-o-rosario1201 <finnnn023@gmail.com>
2024-03-23 17:27:07 -0400

    Test listo

---
+++ TareaTestValidacionContraseña/Contraseña.cs
@@ -0,0 +1,88 @@
+﻿namespace TareaTestValidacionContraseña;
+
+public class Contraseña
+{
+	public void InsertarContraseaña()
+	{
+		string password = "";
+		string patronMayusculas = @"[A-Z]{1,}";
+		string patronMinusculas = @"[a-z]{1,}";
+		string patronDigitos = @"[0-9]{1,}";	
+		string patronCaracteresEspeciales = @"\W";
+
+		VerificarContraseña verificarContraseña = new VerificarContraseña();
+
+		verificarContraseña.VerificarVacio(password);
+		verificarContraseña.VerificarCarecteresEspeciales(password, patronCaracteresEspeciales);
+		verificarContraseña.VerificarLongitud(password);
+		verificarContraseña.VerificarMayusculas(password, patronMayusculas);
+		verificarContraseña.VerificarMinusculas(password, patronMinusculas);
+		verificarContraseña.VerificarDigitosNumericos(password, patronDigitos);
+	}
+
+	public bool VerificarQueNoEsteVacio(string password)
+	{
+		if (String.IsNullOrEmpty(password))
+			return true;
+
+		return false;
+	}
+	public bool VerificarQueNoTengaCaracteresEspeciales(string password)
+	{
+		bool caracterEspecial = false;
+
+		foreach (char caracter in password)
+		{
+			if (char.IsSymbol(caracter) || char.IsPunctuation(caracter))
+				caracterEspecial = true;
+		}
+
+		return caracterEspecial == false;
+	}
+
+	public bool VerificarQueLaLongitudSeaPorLoMenosCincoDigitos(string password)
+	{
+		if(password.Length >= 5)
+			return true;
+
+		return false;
+	}
+
+	public bool VerificarQueTengaPorLoMenosUnaMayuscula(string password)
+	{
+		bool mayuscula = false;
+
+		foreach (char caracter in password)
+		{
+			if (char.IsUpper(caracter))
+				mayuscula = true;
+		}
+
+		return mayuscula;
+	}
+
+	public bool VerificarQueTengaPorLoMenosUnaMinuscula(string password)
+	{
+		bool minuscula = false;
+
+		foreach (char caracter in password)
+		{
+			if (char.IsLower(caracter))
+				minuscula = true;
+		}
+
+		return minuscula;
+	}
+
+	public bool VerificarQueTengaPorLoMenosUnDigitoNumerico(string password)
+	{
+		bool numero = false;
+
+		foreach (char caracter in password)
+		{
+			if (char.IsDigit(caracter))
+				numero = true;
+		}
+		return numero;
+	}
+}
---
+++ TestProjectValidacionContraseña/TestValidacionContraseña.cs
@@ -0,0 +1,104 @@
+using TareaTestValidacionContraseña;
+
+namespace TestProjectValidacionContraseña
+{
+	[TestClass]
+	public class TestValidacionContraseña
+	{
+		[TestMethod]
+		public void Should_Verify_If_The_Password_Is_Empty_Or_Null()
+		{
+			//Arrange
+			var cut = new Contraseña();
+
+			//Act
+			string password = "David123";
+			bool expect = false;
+
+			var result = cut.VerificarQueNoEsteVacio(password);
+
+			//Assert
+			Assert.AreEqual(expect, result);
+		}
+
+		[TestMethod]
+		public void Should_Verify_If_The_Password_Has_Special_Characters()
+		{
+			//Arrange
+			var cut = new Contraseña();
+
+			//Act
+			string password = "David123";
+			bool expect = true;
+
+			var result = cut.VerificarQueNoTengaCaracteresEspeciales(password);
+
+			//Assert
+			Assert.AreEqual(expect, result);
+		}
+
+		[TestMethod]
+		public void Should_Verify_If_The_Password_Has_At_Least_Five_Digits()
+		{
+			//Arrange
+			var cut = new Contraseña();
+
+			//Act
+			string password = "David123";
+			bool expect = true;
+
+			var result = cut.VerificarQueLaLongitudSeaPorLoMenosCincoDigitos(password);
+
+			//Assert
+			Assert.AreEqual(expect, result);
+		}
+
+		[TestMethod]
+		public void Should_Verify_If_The_Password_Has_At_Least_One_Upper_Digit()
+		{
+			//Arrange
+			var cut = new Contraseña();
+
+			//Act
+			string password = "David123";
+			bool expect = true;
+
+			var result = cut.VerificarQueTengaPorLoMenosUnaMayuscula(password);
+
+			//Assert
+			Assert.AreEqual(expect, result);
+		}
+
+		[TestMethod]
+		public void Should_Verify_If_The_Password_Has_At_Least_One_Lower_Digit()
+		{
+			//Arrange
+			var cut = new Contraseña();
+
+			//Act
+			string password = "David123";
+			bool expect = true;
+
+			var result = cut.VerificarQueTengaPorLoMenosUnaMinuscula(password);
+
+			//Assert
+			Assert.AreEqual(expect, result);
+		}
+
+		[TestMethod]
+		public void Should_Verify_If_The_Password_Has_At_Least_One_Numeric_Character()
+		{
+			//Arrange
+			var cut = new Contraseña();
+
+			//Act
+			string password = "David123";
+			bool expect = true;
+
+			var result = cut.VerificarQueTengaPorLoMenosUnDigitoNumerico(password);
+
+			//Assert
+			Assert.AreEqual(expect, result);
+		}
+	}
+}
